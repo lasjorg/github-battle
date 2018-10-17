7478dda7f30d93d3db58edbebf36da6b7d957da8
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-10-17 23:05:39 +0200

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import Home from './Home';
+import Battle from './Battle';
 import Popular from './Popular';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Nav from './Nav';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/battle' component={Battle}/>
+            <Route path='/popular' component={Popular}/>
+            <Route render={ () => {
+              return <p>Life's what you make it</p>
+            }}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
