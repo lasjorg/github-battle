26a74bb147ec4aa06ed168d591e2b6b70a7561a5
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-10-19 22:19:08 +0200

    Update api to get user data for the battle

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,62 @@
 import axios from "axios";
 
+const getProfile = (username) => {
+  return axios.get(`http://api.github/users/${username}`)
+    .then(user => {
+      return user.data;
+    })
+};
+
+const getRepos = (username) => {
+  return axios.get(`http://api.github/users/${username}/repos&per_page=100`);
+};
+
+const getStarCount = (repos) => {
+  return repos.data.reduce( (count, repo) => count + repo.stargazers_count);
+};
+
+const calculateScore = (profile, repos) => {
+  const followers = profile.followers;
+  const totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+};
+
+const handleError = (error) => {
+  console.warn(error);
+  return null;
+}
+
+const getUserData = (player) => {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then( data => {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+const sortPlayers = (players) => {
+  return players.sort( (a,b) => b.score - a.score);
+}
+
+const battle = (players) => {
+   return axios.all(players.map(getUserData))
+     .then(sortPlayers)
+     .catch(handleError)
+}
+
 const fetchPopularRepos = (lang) => {
   const encodeURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${lang}&sort=stars&order=desc&type=Repositories`);
 
   return axios.get(encodeURI)
-    .then(res => res.data.items)
-}
+    .then(res => res.data.items);
+};
 
-export {fetchPopularRepos};
+export {fetchPopularRepos, battle};
