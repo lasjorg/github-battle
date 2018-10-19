2be8eb72505ee3a679fcd5c1d9f7a6df1fbed1a7
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-10-19 22:17:55 +0200

    Add results route

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Battle from './Battle';
+import Results from './Results';
 import Popular from './Popular';
 import Nav from './Nav';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -14,7 +15,8 @@ class App extends Component {
           <Nav />
           <Switch>
             <Route exact path='/' component={Home}/>
-            <Route path='/battle' component={Battle}/>
+            <Route exact path='/battle' component={Battle}/>
+            <Route path='/battle/results' component={Results}/>
             <Route path='/popular' component={Popular}/>
             <Route render={ () => {
               return <p>Life's what you make it</p>
