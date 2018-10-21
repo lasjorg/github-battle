c78045a10aacfd7188f4b69cf9c473bd2c732fef
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-10-21 10:09:50 +0200

    Fix api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const getProfile = (username) => {
-  return axios.get(`http://api.github/users/${username}`)
+  return axios.get(`http://api.github.com/users/${username}`)
     .then(user => {
       return user.data;
     })
 };
 
 const getRepos = (username) => {
-  return axios.get(`http://api.github/users/${username}/repos&per_page=100`);
+  return axios.get(`http://api.github.com/users/${username}/repos?&per_page=100`);
 };
 
 const getStarCount = (repos) => {
-  return repos.data.reduce( (count, repo) => count + repo.stargazers_count);
+  return repos.data.reduce( (count, repo) => count + repo.stargazers_count, 0);
 };
 
 const calculateScore = (profile, repos) => {
